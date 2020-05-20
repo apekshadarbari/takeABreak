f481d2f6abd78abc7ffe783827be239d0f3ca06a
Apeksha Darbari <apekshadarbari@gmail.com>
2020-05-19 22:47:42 -0700

    Fix for copy-paste and accounting rewritten words

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ export class AppComponent {
   /*-------- Variable Definitions
              oldCount => Word count at which last break was called 
              newCount => Word count since the last break
+             totalWordsTyped => Count of all words type (including ones that were deleted)
              breakTime => Time configured for a break (30 seconds in this case)
   -----------*/
 
@@ -22,6 +23,7 @@ export class AppComponent {
   currentWordCount = 0;
   oldCount = 0;
   newCount = 0;
+  totalWordsTyped = 0;
   breakTime = 30000;
   disableInput = false;
 
@@ -35,9 +37,8 @@ export class AppComponent {
   countingWords() {
 
 
-
-      this.newCount = this.wordCounter.transform(this.message) - this.oldCount;
-
+    this.totalWordsTyped++;
+    this.newCount = this.totalWordsTyped - this.oldCount;
     if (this.newCount % 100 === 0 && this.newCount != 0) {
       this.oldCount += this.newCount;
       this.newCount = 0;
@@ -59,3 +60,4 @@ export class AppComponent {
 
 
 }
+
